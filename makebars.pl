4f1adf2a491869ae5b99952277ea690e1c16b35e
wjrice <35811476+wjrice@users.noreply.github.com>
2019-09-12 10:15:41 -0400

    Add files via upload
    newer version of makebars, to add scalebars to images

--- makebars.pl
+++ makebars.pl
@@ -1,17 +1,25 @@
 #! /usr/bin/perl -w
 # needs IMOD environment loaded to work
-my @files = glob("*.mrc");
+my @files = glob("*e?n-?.mrc");
+#my @files = glob("*.mrc");
 my $mydir = "scalebar";
+my $set=0;
+
 unless (-e $mydir) {mkdir $mydir;}
 `ln -s ../mrc2jpg_withscale.py .`;
 `ln -s ../MrcImagePlugin.py .`;
-
+if ($ARGV[0]) {
+   $apix = $ARGV[0];
+   $set=1;
+}
 foreach $file (@files) {
-   $line = `header $file | grep Angstroms`;
+unless ($set) {
+      $line = `header $file | grep Angstroms`;
+      @data = split(" ",$line);
+      $apix = pop (@data);
+}
    $fileout = $file;
    $fileout =~ s/mrc/png/;
-   @data = split(" ",$line);
-   $apix = pop (@data);
    print "$file $apix\n";
    `./mrc2jpg_withscale.py $file $mydir/$fileout $apix`;
 }
